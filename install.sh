abb31be512500cc58d11b921e0d2cebf9ec3afa6
caohungphu <caohungphuvn@gmail.com>
2025-04-01 00:18:51 +0700

    chore: upload phase 6

--- install.sh
+++ install.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
+# Define variables
 export P_REPO="https://github.com/caohungphu/pconfig"
 export P_PATH_REPO="${HOME}/.pconfig"
 export P_PATH_LIBS="${P_PATH_REPO}/libs"
-
-# Clone the repository if it doesn't exist
-if [ ! -d "${P_PATH_REPO}" ]; then
-    git clone ${P_REPO} ${P_PATH_REPO}
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to clone repository."
-        exit 1
-    fi
-else
-    git pull origin main
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to update repository."
-        exit 1
-    fi
-fi
 
 # Load libraries
 source "${P_PATH_LIBS}/central.sh"
 
 # Main
 cinfo "Initalizing pconfig..."
+
+# Clone the repository if it doesn't exist
+if [ ! -d "${P_PATH_REPO}" ]; then
+    git clone ${P_REPO} ${P_PATH_REPO}
+    if [ $? -ne 0 ]; then
+        cerror "Error: Failed to clone repository."
+        exit 1
+    fi
+    cinfo "Repository cloned successfully to ${P_PATH_REPO}."
+else
+    git pull origin main
+    if [ $? -ne 0 ]; then
+        cerror "Failed to update repository."
+        exit 1
+    fi
+    cinfo "Repository updated successfully."
+fi
+
+# Check info
 check_distro
 check_shell
+check_kernel
+check_machine
+
+# Update system
 update_system
+
 cinfo "Initialization complete."
